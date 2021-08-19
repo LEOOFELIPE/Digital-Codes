1d5dbba2b5c21fd027f6ce7af57a291af9050aed
LEOOFELIPE <32253174+LEOOFELIPE@users.noreply.github.com>
2021-08-19 18:34:58 -0300

    Update Q4_B.py

--- Q4_B.py
+++ Q4_B.py
@@ -31,12 +31,6 @@ fig = plt.figure()
 for i in range(n+1):
     u[i] = 1
 
-#Resposta ao Degrau
-
-quais_T=np.linspace(-1, t_final,10)
-G_s = clt.tf([1.],[1.,1.,1.])
-
-T_mf,yout_mf = clt.step_response(G_s,quais_T[quais_T>0]) 
 
 # Configurando o gráfico.
 
@@ -56,16 +50,13 @@ plt.tick_params(which='both', width=0.8)
 plt.tick_params(which='major', length=5)
 plt.tick_params(which='minor', length=2.5)
 
-# Título do gráfico.
-#plt.title(u'Euler versus Analítico, com $\Delta t =$ %4.3f'%(delta_t), fontsize = 24)
 
 # Limites dos eixos na hora de plotar. 
 plt.axis((t_inicial - 0.1, t_final + 0.1, 0.0, 1.5))
 
-# Plotando a solução numérica.
 plt.plot(t, y, '-go',lw = 3, label = 'Discretizado')
 plt.plot(t, u, lw = 3, color = 'blue', label = 'Entrada')
-#plt.plot(T_mf, yout_mf, lw = 3, color = 'k', label = 'Entrada')
+
 plt.title(u'Resposta ao Degrau', fontsize = 24)
 
 # Legenda.
